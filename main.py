65e537215aa2df70a03d5430d204245c97c88e01
Ramya95CDBA924449DF08D994D0FEA39E6C29 <144417042+Ramya95CDBA924449DF08D994D0FEA39E6C29@users.noreply.github.com>
2023-09-15 18:48:55 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-# 1.1 Implement a recursive function to calculate the factorial of a given number
+class Bankaccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name =account_holder_name
+        self.__account_balance = initial_balance
 
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rec(n-1)
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print("Deposited{}. New balance: ₹{}".format(amount, self.__account_balance))
 
-number=2
-res=fact_rec(number)
+        else:
+            print("Invalid deposit amount. Please deposit a positive amount.")
 
-print("The factorial of {} is {}.".format(number,res))
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print("Withdrew ₹{}. New balance: ₹{}".format(amount, self.__account_balance))
+
+        else:
+            print("Invalid withdrawal amount or insufficient balance.")
+
+    def display_balance(self):
+        print("Account balance for {} (Account#{}): ₹{}".format(
+            self.__account_holder_name, self.__account_number,
+            self.__account_balance))
+
+account = Bankaccount(account_number="2201111033007",
+                            account_holder_name="Cheran B",
+                            initial_balance=5000.0)
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.display_balance()
